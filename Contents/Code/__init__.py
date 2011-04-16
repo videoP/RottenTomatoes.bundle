316938b017b271adf1adf787e601746c6b57b034
superpea <scott@plexapp.com>
2011-04-16 12:21:34 -0700

    NEW: move to search function to get RT ID NEW: if imdb search fails, use API to search manually

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -12,13 +12,21 @@ class RottenTomatoesAgent(Agent.Movies):
   
   def search(self, results, media, lang):
     imdb_id = media.primary_metadata.id.replace('tt', '')
-    results.Append(MetadataSearchResult(id=imdb_id, score=100))
+    try:
+      rtID = HTML.ElementFromURL(RT_IMDB_SEARCH_URL % imdb_id).xpath('//div[@id="all"]')[0].get('movie')
+    except:
+      #manual movie search via the API -- requires exact name/year match
+      rtSearch = JSON.ObjectFromURL(RT_API_BASE % ('movies.json', '&q=' + String.Quote(media.primary_metadata.title)))
+      for r in rtSearch['movies']:
+        #Log('rt: ' + r['title'] + ' ' + str(r['year']))
+        #Log('in: ' + media.primary_metadata.title + ' ' + str(media.primary_metadata.year))
+        if media.primary_metadata.title == r['title'] and media.primary_metadata.year == r['year']:
+          rtID = r['id']
+          break
+    if rtID: results.Append(MetadataSearchResult(id=rtID, score=100))
   
   def update(self, metadata, media, lang):
-    try: movie_page = HTML.ElementFromURL(RT_IMDB_SEARCH_URL % metadata.id)
-    except: return
-    rtID = movie_page.xpath('//div[@id="all"]')[0].get('movie')
-    rtMovie = JSON.ObjectFromURL(RT_API_BASE % ('movies/' + rtID + '.json', ''))
+    rtMovie = JSON.ObjectFromURL(RT_API_BASE % ('movies/' + metadata.id + '.json', ''))
     #Log(str(rtMovie))
     #get ratings
     if Prefs['get_rating']:
@@ -59,5 +67,8 @@ class RottenTomatoesAgent(Agent.Movies):
       metadata.roles.clear()
       for c in rtMovie['abridged_cast']:
         role = metadata.roles.new()
-        role.role = c['characters'][0]
+        try: 
+          role.role = c['characters'][0]
+        except:
+          role.role = c['name']
         role.actor = c['name']
