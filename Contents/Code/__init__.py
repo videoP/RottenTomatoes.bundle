f51b933e0f028d24cbbacdd0a9a75de8b7d6dfa3
superpea <scott@plexapp.com>
2011-06-14 23:17:35 -0700

    change to use The Official RT imdb lookup endpoint. should help make sure we don't end up with bogus matches. unless, of course their data is bad, which it has been known to be.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,3 @@
-RT_IMDB_SEARCH_URL = 'http://www.rottentomatoes.com/alias?type=imdbid&s=%s'
 RT_API_BASE = 'http://api.rottentomatoes.com/api/public/v1.0/%s?apikey=twdg3rv2zeesn29thmx58sky%s'
 
 def Start():
@@ -12,23 +11,12 @@ class RottenTomatoesAgent(Agent.Movies):
   
   def search(self, results, media, lang):
     imdb_id = media.primary_metadata.id.replace('tt', '')
-    try:
-      rtID = HTML.ElementFromURL(RT_IMDB_SEARCH_URL % imdb_id).xpath('//div[@id="all"]')[0].get('movie')
-    except:
-      #manual movie search via the API -- requires exact name/year match
-      rtSearch = JSON.ObjectFromURL(RT_API_BASE % ('movies.json', '&q=' + String.Quote(media.primary_metadata.title)))
-      for r in rtSearch['movies']:
-        #Log('rt: ' + r['title'] + ' ' + str(r['year']))
-        #Log('in: ' + media.primary_metadata.title + ' ' + str(media.primary_metadata.year))
-        if media.primary_metadata.title == r['title'] and media.primary_metadata.year == r['year']:
-          rtID = r['id']
-          break
-    try: results.Append(MetadataSearchResult(id=rtID, score=100))
-    except: return
+    rtSearch = JSON.ObjectFromURL(RT_API_BASE % ('movie_alias.json', '&type=imdb&id=' + imdb_id))
+    if not rtSearch.has_key('error'):
+      results.Append(MetadataSearchResult(id=str(rtSearch['id']), score=100))
   
   def update(self, metadata, media, lang):
     rtMovie = JSON.ObjectFromURL(RT_API_BASE % ('movies/' + metadata.id + '.json', ''))
-    #Log(str(rtMovie))
     #get ratings
     if Prefs['get_rating']:
       if Prefs['rating_type'] == 'Tomatometer':
